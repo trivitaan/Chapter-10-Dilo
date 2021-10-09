c7f6ea589675ef18b9f47a5855cf764ff657244e
Trivita Annisa Utami <trivita1900018074@webmail.uad.ac.id>
2021-10-09 22:29:25 +0700

    probs

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -6,13 +6,36 @@ using UnityEngine;
 public class BallMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    public float speed;
+    Vector2 movement;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            SetTargetPosition();
+        }else
+        {
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+            rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        }
+    }
+
     void FixedUpdate()
     {
-        rb.AddForce(new Vector2(60.0f, 35.0f));
+        //rb.AddForce(new Vector2(60.0f, 35.0f));
+        //rb.velocity = new Vector2(20.0f, 6.0f);
+        
+    }
+
+    void SetTargetPosition()
+    {
+        
     }
 }
